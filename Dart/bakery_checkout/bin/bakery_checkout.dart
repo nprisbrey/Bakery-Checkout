981db93364910c139773e28f6ba7dc0489200686
Nathan Prisbrey <ntpbanana@gmail.com>
2021-12-23 23:44:53 -0800

    Added Utility Methods To 'bakery_checkout.dart'

--- Dart/bakery_checkout/bin/bakery_checkout.dart
+++ Dart/bakery_checkout/bin/bakery_checkout.dart
@@ -1,3 +1,15 @@
+void printMenu() {
+  print("""
+    ******** MENU ********
+    Below are the different options that you can enter as a sub-order (all quantities are integers):
+    Layer-cake [flavor] [frosting] [number of layers] [quantity]
+    Cupcake [flavor] [frosting] [sprinkles] [quantity]
+    Bread [variety] [quantity]
+    """);
+}
+
+String makeLowercase(String original) => original.toLowerCase();
+
 void main(List<String> arguments) {
   print('Hello world!');
 }
